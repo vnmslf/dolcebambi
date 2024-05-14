8073295cad8989154e362a0844c4d57a43b9358d
miberezkin <vnmslf@gmail.com>
2024-05-14 11:46:08 +0300

    master: ty page

---
+++ thank-you/index.php
@@ -0,0 +1,8 @@
+<?require_once($_SERVER['DOCUMENT_ROOT'].'/assets/header.php');?>
+<section class="thank-you">
+	<div class="container">
+		<h1>Спасибо!</h1>
+		<p>Ваша заявка принята. Мы скоро свяжемся с вами!</p>
+	</div>
+</section>
+<?require_once($_SERVER['DOCUMENT_ROOT'].'/assets/footer.php');?>
